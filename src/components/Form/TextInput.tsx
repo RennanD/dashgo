173569c709bb602bfd99f2372abae6e888c77771
rennand <rplaykl@gmail.com>
2021-04-01 16:32:13 -0300

    feat: create SignIn page

---
+++ src/components/Form/TextInput.tsx
@@ -0,0 +1,31 @@
+import {
+  Input,
+  FormLabel,
+  FormControl,
+  InputProps as ChakraInputProps,
+} from '@chakra-ui/react';
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+}
+
+export function TextInput({ name, label, ...rest }: InputProps): JSX.Element {
+  return (
+    <FormControl>
+      {label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      <Input
+        id={name}
+        name={name}
+        focusBorderColor="pink-500"
+        background="gray.900"
+        variant="filled"
+        _hover={{
+          background: 'gray.900',
+        }}
+        size="lg"
+        {...rest}
+      />
+    </FormControl>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
-export default function Home(): JSX.Element {
+import { Flex, Button, Stack } from '@chakra-ui/react';
+
+import { TextInput } from '../components/Form/TextInput';
+
+export default function SignIn(): JSX.Element {
   return (
-    <div>
-      <h1>Dashgo</h1>
-    </div>
+    <Flex w="100vw" height="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth="360px"
+        background="gray.800"
+        padding="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <TextInput name="email" label="E-mail" />
+          <TextInput name="password" label="Senha" />
+        </Stack>
+
+        <Button type="submit" marginTop="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
